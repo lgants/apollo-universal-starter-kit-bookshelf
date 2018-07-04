7dd9e90e3ed05c1e371e6b9af45803f22911379f
lgants <lgants@gmail.com>
2018-07-04 12:11:29 -0500

    first commit

--- packages/server/src/sql/connector.js
+++ packages/server/src/sql/connector.js
@@ -1,5 +1,10 @@
 import knex from 'knex';
+import bookshelf from 'bookshelf';
 import * as environments from '../../knexdata';
 
 // eslint-disable-next-line import/namespace
-export default knex(environments[process.env.NODE_ENV || 'development']);
+// export default knex(environments[process.env.NODE_ENV || 'development']);
+
+const env = process.env.NODE_ENV || 'development';
+// eslint-disable-next-line import/namespace
+export default bookshelf(knex(environments[env]));
